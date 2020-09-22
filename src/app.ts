cb5ac4a63522b60a10cb4ef75cd84f0c3da58e61
codersingh27 <codersingh27@gmail.com>
2020-09-22 15:32:13 -0500

    bracnh2 - user setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import "reflect-metadata"
 import * as express from "express";
 import * as cors from "cors";
+import userIndexRouter from "./routes/user-routes/index.route";
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -7,6 +9,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hey you i'm here...")
 })
+
+// all the routes here
+app.use("/apiv1", userIndexRouter)
 
 // this is for 404
 app.use(function (req, res, next) {
